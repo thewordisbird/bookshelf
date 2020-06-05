ebd27d0f5b6f412ee344c1cc3e5257436e84de54
thewordisbird <justin@thewordisbird.dev>
2020-06-04 22:41:14 -0700

    Book details page. Need to handle html data in json response and template the reviews

--- bookshelf/google_books.py
+++ bookshelf/google_books.py
@@ -1,9 +1,10 @@
 
 import requests
 import json
-def get_book(volume_id):
+def get_book(book_id):
     #endpoint = f"https://www.googleapis.com/books/v1/volumes?q={search_data.replace(' ', '+')}"
-    endpoint = f"https://www.googleapis.com/books/v1/volumes/{volume_id}"
-    resp = requests.get(endpoint)
+    endpoint = f"https://www.googleapis.com/books/v1/volumes/{book_id}"
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    resp = requests.get(endpoint, headers=headers)
     return resp.json()
 
--- bookshelf/routes.py
+++ bookshelf/routes.py
@@ -1,13 +1,20 @@
-from flask import Flask, Blueprint, render_template, request
+
+from flask import Flask, Blueprint, render_template, request, jsonify, json
 
 from .forms import SearchForm
 from .google_books import get_book
-
+from html import unescape
 bp = Blueprint('books', __name__)
 
-@bp.route('/search', methods=["GET"])
+@bp.route('/search', methods=['GET'])
 def search():
     form = SearchForm()
     return render_template('search.html', title='Find A Book', form=form)
 
+@bp.route('/book/<book_id>', methods=['GET'])
+def book_details(book_id):
+    book = get_book(book_id)
+    description = unescape(book['volumeInfo']['description'])
+    #print(unescape(book['volumeInfo']['description']))
+    return render_template('book_details.html', book=book, description=description)
 
