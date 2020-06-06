b2245ae79dcb998512395afd4191189274f73b35
thewordisbird <justin@thewordisbird.dev>
2020-06-05 18:51:49 -0700

    Book review form to add to book details

--- bookshelf/forms.py
+++ bookshelf/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField, DateTimeField, IntegerField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     search = StringField('Search', validators=[DataRequired()])
 
+class ReviewForm(FlaskForm):
+    rating = IntegerField('Rating', validators=[DataRequired()])
+    review_title = StringField('Title')
+    review_content = TextAreaField('Review')
+    date_started = DateTimeField('Date Started', format='%m/%d/%Y')
+    date_finished =DateTimeField('Date Finished', format='%m/%d/%Y')
--- bookshelf/routes.py
+++ bookshelf/routes.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, Blueprint, render_template, request, jsonify, json
 
-from .forms import SearchForm
+from .forms import SearchForm, ReviewForm
 from .google_books import get_book
 from html import unescape
 bp = Blueprint('books', __name__)
@@ -16,6 +16,16 @@ def book_details(book_id):
     book = get_book(book_id)
     #print(unescape(book['volumeInfo']['description']))
     return render_template('book_details.html', book=book)
+
+@bp.route('/review/new/<book_id>', methods=['GET', 'POST'])
+def new_review(book_id):
+    form = ReviewForm()
+    book = get_book(book_id)
+    if form.validate_on_submit():
+        pass
+
+    return render_template('review_form.html', form=form, book=book)
+
 
 @bp.route('/test')
 def test():
